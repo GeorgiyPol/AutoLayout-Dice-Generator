34c711b8505d4f3b77a0ba17c199ccb95a64e777
Georgiy <33875528+georg1856@users.noreply.github.com>
2022-09-01 20:55:29 +1000

    Create logic operations

--- AutoLayout-Dice-Generator/AutoLayout-Dice-Generator/ViewController.swift
+++ AutoLayout-Dice-Generator/AutoLayout-Dice-Generator/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageViewOne: UIImageView!
+    @IBOutlet weak var diceImageViewTwo: UIImageView!
+    
+    let diceArray = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        diceImageViewOne.image = diceArray[Int.random(in: 0...5)]
+        diceImageViewTwo.image = diceArray[Int.random(in: 0...5)]
+    }
+    
 }
 
